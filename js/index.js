6414912a47b0e0d44f9861b6f47f75554005f235
Susann <susannthue@live.no>
2026-05-08 22:17:36 +0200

    Adjustments carousel
    + adjustment cta styling

--- js/index.js
+++ js/index.js
@@ -1,20 +1,53 @@
 // Carousel
 
-const carouselImages = document.getElementById("carousel-images")
+const carouselImages = document.getElementById("carousel-images");
+const previousButton = document.querySelector(".previous-button");
+const nextButton = document.querySelector(".next-button");
+
+let carouselProducts = [];
+let currentIndex = 0;
 
 getProducts().then(products => {
     
-    const carouselProducts = products.slice(13,16);
+    carouselProducts = products.slice(13,16);
 
-    carouselProducts.forEach(product => {
+    showProduct(currentIndex);
+
+});
+
+function showProduct(index) {
+
+    const product = carouselProducts[index];
     
-        carouselImages.innerHTML +=`
+        carouselImages.innerHTML =`
             <a href="product/index.html?id=${product.id}">
                 <img src="${product.image.url}" alt="${product.title}">
             </a>
         `;
-    });
+}
+
+nextButton.addEventListener("click", () => {
+
+    currentIndex++;
+
+    if (currentIndex >= carouselProducts.length) {
+        currentIndex = 0;
+    }
+
+    showProduct(currentIndex);
 });
+
+previousButton.addEventListener("click", () => {
+
+    currentIndex--;
+
+    if (currentIndex < 0) {
+        currentIndex = carouselProducts.length - 1;
+    }
+
+    showProduct(currentIndex);
+});
+
 
 
 // Product List
