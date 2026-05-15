d1d11ee9f25c435d8950729f4a6b2ecec06f4357
Susann <susannthue@live.no>
2026-05-15 22:11:08 +0200

    Account setup
    Account register and login setup js
    + minor adjustments form

---
+++ js/login.js
@@ -0,0 +1,7 @@
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    console.log("Submit test");
+});
---
+++ js/register.js
@@ -0,0 +1,7 @@
+const registerForm = document.querySelector(".register-form");
+
+registerForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    console.log("Submit test");
+});
