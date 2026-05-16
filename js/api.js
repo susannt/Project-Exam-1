cdd65c21089cc6507ac0496e9d1466cef8e3476a
Susann <susannthue@live.no>
2026-05-16 12:52:14 +0200

    Register account logic
    Register account logic + api (post)

--- js/api.js
+++ js/api.js
@@ -1,7 +1,8 @@
 const productsUrl = "https://v2.api.noroff.dev/online-shop";
-const loginUrl = "https://v2.api.noroff.dev/auth/login";
 const registerUrl = "https://v2.api.noroff.dev/auth/register";
+const loginUrl = "https://v2.api.noroff.dev/auth/login";
 
+// All products
 
 async function getProducts() {
   const response = await fetch(productsUrl);
@@ -9,8 +10,29 @@ async function getProducts() {
   return result.data;
 }
 
+// Products by id
+
 async function getProduct(id) {
   const response = await fetch(`${productsUrl}/${id}`);
   const product = await response.json();
   return product.data;
+}
+
+//Register account
+
+async function registerAccount(name, email, password) {
+  const response = await fetch(registerUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      name,
+      email,
+      password
+    })
+  });
+
+  const result = await response.json();
+  return result;
 }
--- js/register.js
+++ js/register.js
@@ -1,7 +1,20 @@
 const registerForm = document.querySelector(".register-form");
 
-registerForm.addEventListener("submit", function(event) {
+registerForm.addEventListener("submit", async function(event) {
     event.preventDefault();
 
-    console.log("Submit test");
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+
+    const result = await registerAccount(name, email, password);
+
+    console.log(result);
+
+    if (result.data) {
+        alert("Account created!");
+        window.location.href = "./login.html";
+    } else {
+        alert(result.errors?.[0]?.message || "Could not create account");
+    }
 });
